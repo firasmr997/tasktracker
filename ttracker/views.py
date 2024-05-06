bdeb3732bcae268071cdb7949341a340b05e8931
Firas <firasmr997@gmail.com>
2024-05-06 09:25:24 +0100

    intgrate template

--- ttracker/views.py
+++ ttracker/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
 # Create your views here.
+from django.shortcuts import render, redirect
+from django.views.generic import TemplateView
+
+from .models import *
+
+
+def home(request):
+    return render(request, 'pages/home.html')
+
+
+def login(request):
+    return render(request, 'pages/login.html')
